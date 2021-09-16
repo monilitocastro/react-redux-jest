2d54a70cff0587f72b3d4867c1133cfc9815757f
Monilito Castro <mannycastro.epic@gmail.com>
2021-09-16 17:52:42 -0400

    actions made accessible to intellisense

--- src/reducers.js
+++ src/reducers.js
@@ -1,14 +1,19 @@
-import {ADD_ONE, MINUS_ONE} from './actions';
 const initialState = {
     number: 0
 };
+
+export const MainActions = {
+    ADD_ONE: "ADD_ONE",
+    MINUS_ONE: "MINUS_ONE"
+}
+
 function reducer(state = initialState, action) {
     switch (action.type) {
-        case ADD_ONE:
+        case MainActions.ADD_ONE:
             return {
                 number: state.number + 1
             };
-        case MINUS_ONE:
+        case MainActions.MINUS_ONE:
             return {
                 number: state.number - 1
             };
--- src/tests/store.test.js
+++ src/tests/store.test.js
@@ -1,5 +1,5 @@
 import {createStore} from 'redux';
-import reducer from '../reducers';
+import reducer, {MainActions} from '../reducers';
 
 describe('Redux', () => {
     let store = null;
@@ -10,7 +10,7 @@ describe('Redux', () => {
         expect(store && typeof store === 'object').toBe(true)
     })
     it("increments", async () => {
-        await store.dispatch({type: "ADD_ONE"})
+        await store.dispatch({type: MainActions.ADD_ONE})
         const state = store.getState();
         expect(state && typeof state === 'object').toBe(true)
         expect(state.number).toEqual(1);
